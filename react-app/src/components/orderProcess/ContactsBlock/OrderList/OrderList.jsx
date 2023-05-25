afc17c77d9212a0e22906ae4830c1671575ed136
Yaroslav Chaban <keannko@gmail.com>
2023-05-26 00:52:36 +0400

    fix orderlist

--- react-app/src/components/orderProcess/ContactsBlock/OrderList/OrderList.jsx
+++ react-app/src/components/orderProcess/ContactsBlock/OrderList/OrderList.jsx
@@ -1,6 +1,4 @@
-import {
-  List, ListItem, ListItemIcon, ListItemText, Typography, Container, Grid
-} from '@mui/material';
+import { Typography, Container, Grid } from '@mui/material';
 
 const OrderList = () => (
   <Container>
@@ -39,31 +37,3 @@ const OrderList = () => (
 );
 
 export default OrderList;
-
-{ /* <List
-      sx={{
-        // width: '100%',
-        // maxWidth: 500,
-        // bgcolor: 'background.paper',
-        // position: 'relative',
-        overflow: 'auto',
-        maxHeight: 300,
-        '& ul': { padding: 0 }
-      }}
-    >
-      {[0, 1, 2].map(sectionId => (
-        <li key={`section-${sectionId}`}>
-          <ul>
-            {[0, 1].map(item => (
-              <ListItem key={`item-${sectionId}-${item}`}>
-                <ListItemIcon>
-                  <img src="./orderprocessTest/3.png" alt="" />
-                </ListItemIcon>
-                <ListItemText primary="Велсон таблетки 30 шт." />
-                <ListItemText primary="108 грн." />
-              </ListItem>
-            ))}
-          </ul>
-        </li>
-      ))}
-    </List> */ }
