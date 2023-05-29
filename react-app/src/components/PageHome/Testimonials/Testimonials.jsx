244761fbb920e8460978f6077780d4451b2ab66e
Name Surname <email@gmail.com>
2023-05-29 07:54:36 +0300

    main

--- react-app/src/components/PageHome/Testimonials/Testimonials.jsx
+++ react-app/src/components/PageHome/Testimonials/Testimonials.jsx
@@ -1,6 +1,44 @@
-import { Box } from '@mui/material';
-import { wrapperForTestimonials } from './style';
+import {Box, Typography} from '@mui/material';
+import {
+  ave,
+  aveRate,
+  aveText,
+  feedBack,
+  feedBackItem,
+  mainFeedback,
+  totalCountFeed,
+  totalRate,
+  wrapperForTestimonials
+} from './style';
+import {healthBlogStyled} from "../HealthBlog/style/index.js";
 
-const Testimonials = () => <Box sx={wrapperForTestimonials}>Testimonials</Box>;
+const Testimonials = () => <Box sx={wrapperForTestimonials}>
+  <Typography fontFamily="Roboto" component="div" sx={mainFeedback}>
+    <Typography fontFamily="Roboto" component="div" sx={ave}>
+      <Typography fontFamily="Roboto" component="div" sx={aveText}>
+      </Typography>
+      <Typography fontFamily="Roboto" component="div" sx={aveRate}>
+      </Typography>
+    </Typography>
+    <Typography fontFamily="Roboto" component="div" sx={totalRate}>
+    </Typography>
+
+  </Typography>
+  <Typography fontFamily="Roboto" component="div" sx={feedBack}>
+    <Typography fontFamily="Roboto" component="div" sx={feedBackItem}>
+
+    </Typography>
+    <Typography fontFamily="Roboto" component="div" sx={feedBackItem}>
+
+    </Typography>
+    <Typography fontFamily="Roboto" component="div" sx={feedBackItem}>
+
+    </Typography>
+    <Typography fontFamily="Roboto" component="div" sx={totalCountFeed}>
+
+    </Typography>
+
+  </Typography>
+</Box>;
 
 export default Testimonials;
--- react-app/src/components/PageHome/Testimonials/style/index.js
+++ react-app/src/components/PageHome/Testimonials/style/index.js
@@ -3,3 +3,12 @@ export const wrapperForTestimonials = {
   height: '500px',
   marginBottom: '30px'
 };
+
+export const mainFeedback = {}
+export const feedBack = {}
+export const ave = {}
+export const aveText = {}
+export const aveRate = {}
+export const totalRate = {}
+export const feedBackItem = {}
+export const totalCountFeed = {}
