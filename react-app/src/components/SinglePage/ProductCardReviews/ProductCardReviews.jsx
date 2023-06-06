7e5772d711cda064657f48798746741d1e079691
Artem Shchelinskyi <shchelinskyi@gmail.com>
2023-06-06 09:10:37 +0300

    add task5

--- react-app/src/components/SinglePage/ProductCardReviews/ProductCardReviews.jsx
+++ react-app/src/components/SinglePage/ProductCardReviews/ProductCardReviews.jsx
@@ -1,12 +1,152 @@
-import { Box, Typography } from '@mui/material';
-import React from 'react';
+import {Box, Button, Rating, Stack, TextField, Typography} from '@mui/material';
+import React, {useState} from 'react';
+import {useFormik} from 'formik';
+import * as Yup from 'yup';
+import Review from "../Review/index.js";
 
-const ProductCardReviews = () => (
-  <Box>
-    <Typography variant="h4" component="h4" gutterBottom sx={{}}>
-      Відгуки
-    </Typography>
-  </Box>
-);
+const Form = () => {
 
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [review, setReview] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Действия при отправке формы
+        console.log('Name:', name);
+        console.log('Email:', email);
+        console.log('Review:', review);
+    };
+
+    const formik = useFormik({
+        initialValues: {
+            name,
+            email,
+            review
+        },
+        validationSchema: Yup.object({
+            name: Yup.string()
+                .min(2, 'The name must contain at least 2 characters')
+                .max(50, 'Must be 20 characters or less')
+                .required('Required'),
+            email: Yup.string()
+                .email('Невірний формат електронної пошти')
+                .required('Required'),
+            review: Yup.string()
+                .min(10, 'Відгук повинен мати мінімум 10 символів')
+                .max(500, 'Відгук не повинен перевищувати 500 символів')
+                .required('Required')
+        }),
+
+    });
+
+    return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <Stack direction="row" useFlexGap flexWrap="wrap"  justifyContent="space-between"   spacing={3}>
+                    <TextField
+                        label="Ваше ім'я"
+                        value={name}
+                        onChange={(event) => setName(event.target.value)}
+                        fullWidth
+                        margin="normal"
+                        sx={{width: '47%', '& .MuiOutlinedInput-root': {
+                                borderRadius: '20px',
+                            },}}
+                    />
+                    <TextField
+                        label="Ваш email"
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                        fullWidth
+                        margin="normal"
+                        sx={{width: '47%', '& .MuiOutlinedInput-root': {
+                                borderRadius: '20px',
+                            },}}
+                    />
+
+                    <TextField
+                        label="Відгук"
+                        value={review}
+                        onChange={(event) => setReview(event.target.value)}
+                        fullWidth
+                        margin="normal"
+                        multiline
+                        rows={4}
+                        sx={{'& .MuiOutlinedInput-root': {
+                                borderRadius: '20px',
+                            },}}
+
+                    />
+                    <Button type="submit" variant="contained" color="primary" sx={{padding:"18px 61px", fontSize:'12px', color:'#ffffff', fontWeight: '700', borderRadius:'50px'}}>
+                        Відправити
+                    </Button>
+                </Stack>
+
+
+            </form>
+        </>
+    )
+};
+
+const ProductCardReviews = ({productItem}) => {
+    const [value, setValue] = useState(2);
+
+
+    return (
+        <Box>
+            <Typography
+                variant="h4"
+                component="h4"
+                gutterBottom
+                sx={{mt: '68px', mb: '30px', fontSize: '36px', lineHeight: '42px', fontWeight: '700'}}
+            >
+                Відгуки
+            </Typography>
+
+            <Box
+                component="form"
+                sx={{
+                    borderRadius: '20px',
+                    backgroundColor: '#F7FAFB',
+                    padding: '30px'
+                }}
+                noValidate
+                autoComplete="off"
+            >
+                <Typography
+                    variant="h5"
+                    component="h5"
+                    gutterBottom
+                    sx={{mb: '30px', fontSize: '18px', lineHeight: '18px', fontWeight: '500'}}
+                >
+                    Залишити відгук
+                </Typography>
+                <Stack>
+                    <Typography
+                        variant="p"
+                        component="p"
+                        gutterBottom
+                        sx={{fontSize: '14px', lineHeight: '16px', fontWeight: '500'}}
+                    >
+                        Оцінка
+                    </Typography>
+                    <Rating
+                        name="simple-controlled"
+                        value={value}
+                        onChange={(event, newValue) => {
+                            setValue(newValue);
+                        }}
+                        sx={{fontSize: '25px'}}
+                    />
+                    <Form/>
+                </Stack>
+                <Review/>
+
+            </Box>
+        </Box>
+    )
+
+
+};
 export default ProductCardReviews;
---
+++ react-app/src/components/SinglePage/Review/Review.jsx
@@ -0,0 +1,88 @@
+import {useParams} from "react-router-dom";
+import React, {useEffect, useState} from "react";
+import {Avatar, Typography, Stack, Rating, Box} from "@mui/material";
+import ThumbUpOutlinedIcon from '@mui/icons-material/ThumbUpOutlined';
+import ThumbDownOutlinedIcon from '@mui/icons-material/ThumbDownOutlined';
+
+
+const Review = () => {
+    const {id} = useParams();
+    const [productReviews, setProductReviews] = useState(null);
+
+
+    const getReviews = async url => {
+        try {
+            const response = await fetch(url);
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const result = await response.json();
+            return result;
+        } catch (error) {
+            // eslint-disable-next-line no-console
+            console.log('Error fetching products:', error);
+            return null;
+        }
+    };
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const allDataReviews = await getReviews('../reviews.json');
+
+            const reviewsThisProduct = allDataReviews.find((item) => item.id === id);
+
+            setProductReviews(reviewsThisProduct);
+        }
+        fetchData();
+
+    }, [id])
+
+    return (
+        <Box sx={{mt:"40px"}}>
+            {!!productReviews && productReviews.reviews.map((item, index) => {
+                const date = new Date(Number(item.dateReview));
+                const year = date.getFullYear();
+                const month = String(date.getMonth() + 1).padStart(2, '0');
+                const day = String(date.getDate()).padStart(2, '0');
+                const formattedDate = `${day}.${month}.${year}`;
+
+                // new Date(item.dateReview);
+
+
+                return <Stack key={index} sx={{mb:"40px"}}>
+                    <Stack direction="row"  justifyContent="space-between" sx={{mb:"20px"}}>
+                        <Stack direction="row" spacing={2} alignItems="center" sx={{ width:'200px'}}>
+                            <Avatar alt="Remy Sharp" src={item.icon} sx={{width: 56, height: 56}}/>
+                            <Typography variant="p" component="p" gutterBottom sx={{mb: '30px', fontSize: '18px', lineHeight: '18px', fontWeight: '500', mr: "10px"}}>
+                                {item.authorName}
+                            </Typography>
+                        </Stack>
+                        <Stack direction="row" spacing={2} alignItems="center" sx={{paddingRight: "10px"}}>
+                            <Rating name="read-only" value={item.rating} readOnly/>
+                            <Typography variant="p" component="p" gutterBottom sx={{mb: '30px', fontSize: '18px', lineHeight: '18px', fontWeight: '500', mr: "10px"}}>
+                                {formattedDate}
+                            </Typography>
+                        </Stack>
+                        <Stack direction="row" spacing={2} alignItems="center" sx={{ paddingRight: "10px"}}>
+                            <Stack direction="row" spacing={2} alignItems="center" >
+                                <ThumbUpOutlinedIcon/>
+                                <Typography variant="p" component="p" gutterBottom sx={{ fontSize: '16px', lineHeight: '16px', fontWeight: '400'}}>
+                                    {item.countLike}
+                                </Typography>
+                            </Stack>
+                            <Stack direction="row" spacing={2} alignItems="center" >
+                                <ThumbDownOutlinedIcon/>
+                                <Typography variant="p" component="p" gutterBottom sx={{ fontSize: '16px', lineHeight: '16px', fontWeight: '400'}}>
+                                    {item.countDislike}
+                                </Typography>
+                            </Stack>
+                        </Stack>
+                    </Stack>
+                    <Box sx={{textAlign:'justify'}}>{item.mainText}</Box>
+                </Stack>
+            })}
+        </Box>
+    )
+}
+
+export default Review;
