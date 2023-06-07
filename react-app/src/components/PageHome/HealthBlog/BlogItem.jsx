304fd0770240145080bf2232bb8815fa4363e291
Name Surname <email@gmail.com>
2023-06-07 16:11:44 +0300

    Blog

--- react-app/src/components/PageHome/HealthBlog/BlogItem.jsx
+++ react-app/src/components/PageHome/HealthBlog/BlogItem.jsx
@@ -1,9 +1,18 @@
-import {Box} from "@mui/material";
+import {blogDesc, blogImg, blogTitle, Item} from "./style/index.js";
+import {Typography} from "@mui/material";
 
-const BlogItem = () => (
-  <Box>
-
-  </Box>
+const BlogItem = ({ BlogImg, BlogImgName }) => (
+  <Item>
+    <Typography component="div" sx={blogImg}>
+    <img src={BlogImg} alt={`post-${BlogImgName}`} />
+    </Typography>
+    <Typography component="p" sx={blogTitle}>
+      Активная жизнь без "приливов" - все в ваших руках
+    </Typography>
+    <Typography component="p" sx={blogDesc}>
+      И все же природу обмануть невозможно, и почти каждая женщина после сорока лет задумывается о приближении климакса.
+    </Typography>
+  </Item>
 );
 
 export default BlogItem;
--- react-app/src/components/PageHome/HealthBlog/style/index.js
+++ react-app/src/components/PageHome/HealthBlog/style/index.js
@@ -4,26 +4,76 @@ import { styled } from '@mui/material/styles';
 export const healthBlogStyled = {
   fontSize: 36,
   fontWeight: 700,
-  color: '#333333'
+  color: '#333333',
+  textAlign: 'left',
+};
+
+export const toBlog = {
+  position: 'relative',
+  fontSize: 14,
+  color: '#2FD3AE',
+  alignSelf: 'center',
+  '::before': {
+    position: 'absolute',
+    top: '-1px',
+    left: '-14px',
+    content: 'url("../../../../src/assets/right.png")',
+    width: '10px',
+    height: '10px'
+  }
+};
+export const Title = {
+  display: 'flex',
+  gap: '35px',
 };
 
 export const wrapperForItem = {
   display: 'flex',
   justifyContent: 'center',
+  width: '100%',
   marginBottom: '30px'
 };
 
 export const stack = {
-  justifyContent: 'space-between',
-  width: '100%'
+  display: 'flex',
+  justifyContent: 'center',
+  gap:'1.1%',
+  width: '100%',
+  flexWrap: 'wrap',
 };
+
+export const blogTitle = {
+  fontWeight: 700,
+  fontSize: 18,
+  textAlign: 'left',
+  color: '#333333',
+
+};
+
+export const blogDesc = {
+  fontSize: 14,
+  textAlign: 'left',
+  color: '#828282',
+};
+
+export const blogImg = {
+  marginLeft: '-9px',
+}
+
+export const AfterBlogImg = {
+  display: 'flex',
+  alignItems: 'center',
+  flexDirection: 'column',
+}
 
 export const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  textAlign: 'center',
   color: theme.palette.text.secondary,
-  width: '360px',
-  height: '375px'
+  width: '290px',
+  height: '375px',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '20px',
 }));
