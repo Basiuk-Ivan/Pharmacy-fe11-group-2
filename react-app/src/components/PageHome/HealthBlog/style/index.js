0c0d02a06a57cd9fb7340b67c65003094ac9e34a
IvanBasss <122455382+Basiuk-Ivan@users.noreply.github.com>
2023-06-09 14:04:36 +0300

    fix bags

--- react-app/src/components/PageHome/HealthBlog/style/index.js
+++ react-app/src/components/PageHome/HealthBlog/style/index.js
@@ -5,7 +5,7 @@ export const healthBlogStyled = {
   fontSize: 36,
   fontWeight: 700,
   color: '#333333',
-  textAlign: 'left',
+  textAlign: 'left'
 };
 
 export const toBlog = {
@@ -26,7 +26,7 @@ export const toBlog = {
 export const Title = {
   display: 'flex',
   gap: '35px',
-  marginBottom: '30px',
+  marginBottom: '30px'
 };
 
 export const wrapperForItem = {
@@ -39,35 +39,34 @@ export const wrapperForItem = {
 export const stack = {
   display: 'flex',
   justifyContent: 'center',
-  gap:'1.1%',
+  gap: '1.1%',
   width: '100%',
-  flexWrap: 'wrap',
+  flexWrap: 'wrap'
 };
 
 export const blogTitle = {
   fontWeight: 700,
   fontSize: 18,
   textAlign: 'left',
-  color: '#333333',
-
+  color: '#333333'
 };
 
 export const blogDesc = {
   fontSize: 14,
   textAlign: 'left',
-  color: '#828282',
+  color: '#828282'
 };
 
 export const blogImg = {
-  marginLeft: '-9px',
-}
+  marginLeft: '-9px'
+};
 
 export const AfterBlogImg = {
   display: 'flex',
   alignItems: 'center',
   flexDirection: 'column',
-  marginBottom: '80px',
-}
+  marginBottom: '80px'
+};
 
 export const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -78,5 +77,5 @@ export const Item = styled(Paper)(({ theme }) => ({
   height: '375px',
   display: 'flex',
   flexDirection: 'column',
-  gap: '20px',
+  gap: '20px'
 }));
--- react-app/src/pages/ProductPage/ProductPage.jsx
+++ react-app/src/pages/ProductPage/ProductPage.jsx
@@ -1,20 +1,36 @@
 import { useState, useEffect } from 'react';
-import { Container, Typography, Box, Grid, List, ListItemText, ListItemButton } from '@mui/material';
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
-import { useParams } from 'react-router-dom';
-import { ThemeProvider, createTheme, styled } from '@mui/material/styles';
-import ProductCardMainBlock from '../../components/SinglePage/ProductCardMainBlock';
-import ProductCardInstruction from '../../components/SinglePage/ProductCardInstruction';
-import ProductAnalogiesCardContainer from '../../components/SinglePage/ProductAnalogiesCardContainer';
-import ProductCardReviews from '../../components/SinglePage/ProductCardReviews';
 
-const AntTabs = styled(Tabs)({
-  '& .MuiTabs-indicator': {
-    backgroundColor: '#F2C94C'
-  }
-});
+import {
+  Container,
+  Typography,
+  // Box,
+  Grid,
+  List,
+  ListItemText,
+  ListItemButton
+} from '@mui/material';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+// import Tabs from '@mui/material/Tabs';
+// import Tab from '@mui/material/Tab';
+import {
+  useParams
+  // useLocation
+} from 'react-router-dom';
+import {
+  ThemeProvider,
+  createTheme
+  // styled
+} from '@mui/material/styles';
+// import ProductCardMainBlock from '../../components/SinglePage/ProductCardMainBlock';
+// import ProductCardInstruction from '../../components/SinglePage/ProductCardInstruction';
+// import ProductAnalogiesCardContainer from '../../components/SinglePage/ProductAnalogiesCardContainer';
+// import ProductCardReviews from '../../components/SinglePage/ProductCardReviews';
+
+// const AntTabs = styled(Tabs)({
+//   '& .MuiTabs-indicator': {
+//     backgroundColor: '#F2C94C'
+//   }
+// });
 
 const theme = createTheme({
   typography: {
@@ -33,72 +49,63 @@ const theme = createTheme({
   }
 });
 
-const TabPanel = props => {
-  const { children, value, index, ...other } = props;
+// const TabPanel = props => {
+//   const { children, value, index, ...other } = props;
 
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography component="div">{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-};
-const a11yProps = index => ({
-  id: `simple-tab-${index}`,
-  'aria-controls': `simple-tabpanel-${index}`
-});
+//   return (
+//     <div
+//       role="tabpanel"
+//       hidden={value !== index}
+//       id={`simple-tabpanel-${index}`}
+//       aria-labelledby={`simple-tab-${index}`}
+//       {...other}
+//     >
+//       {value === index && (
+//         <Box sx={{ p: 3 }}>
+//           <Typography component="div">{children}</Typography>
+//         </Box>
+//       )}
+//     </div>
+//   );
+// };
+// const a11yProps = index => ({
+//   id: `simple-tab-${index}`,
+//   'aria-controls': `simple-tabpanel-${index}`
+// });
 
 const ProductPage = () => {
-  const { id } = useParams();
-  const [products, setProducts] = useState([]);
-  const [goods, setGoods] = useState(null);
-  const [value, setValue] = useState(0);
-
-  const getProducts = async url => {
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      const result = await response.json();
-      return result;
-    } catch (error) {
-      // eslint-disable-next-line no-console
-      console.log('Error fetching products:', error);
-      return null;
-    }
-  };
+  const { id, category } = useParams();
+  const [product, setProduct] = useState(null);
+  // const [value, setValue] = useState(0);
 
   useEffect(() => {
-    const fetchData = async () => {
-      const cardsData = await getProducts('../products.json');
-      setProducts([...cardsData]);
+    const fetchProduct = async () => {
+      try {
+        // const response = await fetch('../products.json');
+        const response = await fetch(`http://localhost:3004/api/product?categories=${category}`);
+
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const productData = await response.json();
+        const selectedProduct = productData.find(item => item.id === id);
+        setProduct(selectedProduct);
+      } catch (error) {
+        console.error('Error fetching product:', error);
+      }
     };
 
-    fetchData();
+    fetchProduct();
+  }, [category, id]);
 
-    const product = products.find(item => item.id === id);
-
-    setGoods(product);
-  }, [id, products]);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+  // const handleChange = (event, newValue) => {
+  //   setValue(newValue);
+  // };
 
   return (
     <ThemeProvider theme={theme}>
       <div>
-        {!!goods && (
+        {!!product && (
           <Container sx={{ width: '1200px' }}>
             <Grid container spacing={2}>
               <Grid item lg={12}>
@@ -125,14 +132,14 @@ const ProductPage = () => {
                           color: '#2FD3AE'
                         }
                       }}
-                      primary={`${goods?.mainCategory} /`}
+                      primary={`${product?.mainCategory} /`}
                     />
                   </ListItemButton>
                   <ListItemButton sx={{ flex: '0 0 auto', paddingLeft: '5px', paddingRight: 0 }}>
                     <ListItemText
                       sx={{ paddingRight: 0 }}
                       primaryTypographyProps={{ style: { fontSize: '14px', paddingRight: '12px' } }}
-                      primary={`${goods?.subCategory}`}
+                      primary={`${product?.subCategory}`}
                     />
                   </ListItemButton>
                 </List>
@@ -143,10 +150,10 @@ const ProductPage = () => {
                   sx={{ fontSize: '36px', fontWeight: 700, color: '#394045' }}
                   gutterBottom
                 >
-                  {goods?.name}
+                  {product?.name}
                 </Typography>
               </Grid>
-              <Grid item lg={12}>
+              {/* <Grid item lg={12}>
                 <Box sx={{ width: '100%' }}>
                   <Box sx={{ borderBottom: 1, borderColor: 'divider', backgroundColor: '#F7FAFB' }}>
                     <AntTabs value={value} onChange={handleChange} aria-label="basic tabs example">
@@ -158,25 +165,25 @@ const ProductPage = () => {
                     </AntTabs>
                   </Box>
                   <TabPanel value={value} index={0}>
-                    <ProductCardMainBlock productItem={goods} />
-                    <ProductCardInstruction productItem={goods} />
-                    <ProductAnalogiesCardContainer productItem={goods} />
-                    <ProductCardReviews productItem={goods} />
+                    <ProductCardMainBlock productItem={product} />
+                    <ProductCardInstruction productItem={product} />
+                    <ProductAnalogiesCardContainer productItem={product} />
+                    <ProductCardReviews productItem={product} />
                   </TabPanel>
                   <TabPanel value={value} index={1}>
-                    <ProductCardMainBlock productItem={goods} />
-                    <ProductCardInstruction productItem={goods} />
+                    <ProductCardMainBlock productItem={product} />
+                    <ProductCardInstruction productItem={product} />
                   </TabPanel>
                   <TabPanel value={value} index={2}>
-                    <ProductCardMainBlock productItem={goods} />
-                    <ProductAnalogiesCardContainer productItem={goods} />
+                    <ProductCardMainBlock productItem={product} />
+                    <ProductAnalogiesCardContainer productItem={product} />
                   </TabPanel>
                   <TabPanel value={value} index={3}>
-                    <ProductCardMainBlock productItem={goods} />
-                    <ProductCardReviews productItem={goods} />
+                    <ProductCardMainBlock productItem={product} />
+                    <ProductCardReviews productItem={product} />
                   </TabPanel>
                 </Box>
-              </Grid>
+              </Grid> */}
             </Grid>
           </Container>
         )}
