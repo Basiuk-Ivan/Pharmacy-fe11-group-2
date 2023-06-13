22d03ec5873af13d35a85032ad66fa36de2b0e10
Yaroslav Chaban <keannko@gmail.com>
2023-06-14 02:54:54 +0400

    fix

--- react-app/src/components/orderProcess/ContactsBlock/ContactsForm/ContactsForm.jsx
+++ react-app/src/components/orderProcess/ContactsBlock/ContactsForm/ContactsForm.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 /* eslint-disable no-console */
@@ -30,9 +31,9 @@ const ContactsForm = () => {
       .required('Введіть своє прізвище кирилицею')
       .matches(nameRegExp, 'Введіть своє прізвище кирилицею')
       .min(2, 'Мінімум два символи'),
-    phone: Yup.number().required('Введіть номер мобільного телефону'),
+    phone: Yup.number().required('Введіть номер мобільного телефону').typeError('Введіть номер мобільного телефону'),
     street: Yup.string().required('Введіть назву вулиці'),
-    apartment: Yup.number().required('Введіть номер квартири')
+    apartment: Yup.number().required('Введіть номер квартири').typeError('Введіть номер квартири')
   });
 
   const formik = useFormik({
