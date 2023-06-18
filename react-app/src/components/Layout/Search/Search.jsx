c20d05ef70516b20ff261e2fe14985045cdf4da6
Artem Shchelinskyi <shchelinskyi@gmail.com>
2023-06-18 15:13:02 +0300

    fix cart

--- react-app/src/components/Layout/Search/Search.jsx
+++ react-app/src/components/Layout/Search/Search.jsx
@@ -1,9 +1,10 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { CircularProgress, Stack, Box, Divider } from '@mui/material';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import SearchIcon from '@mui/icons-material/Search';
 import ClearIcon from '@mui/icons-material/Clear';
 import { NavLink } from 'react-router-dom';
+import axios from 'axios';
 import {
   Search,
   SearchIconWrapper,
@@ -18,14 +19,13 @@ import {
   listIconStyle,
   searchBlockInnerStyle
 } from './style';
-import axios from 'axios';
 
 const SearchActions = () => {
   const [text, setText] = useState('');
   const [products, setProducts] = useState([]);
   const [isFetch, setIsFetch] = useState(false);
-  let timeoutId;
-  let isFetchId;
+  const timeoutIdRef = useRef(null);
+  const isFetchIdRef = useRef(null);
 
   useEffect(() => {
     const responseDelay = 300;
@@ -36,10 +36,10 @@ const SearchActions = () => {
         const url = `http://localhost:3004/api/product?search=${text}`;
         const response = await axios.get(url);
 
-        const prods = response.data.prods;
+        const { prods } = response.data;
         setProducts(prods);
         if (!isFetch) {
-          isFetchId = setTimeout(() => setIsFetch(true), noItemsDelay);
+          isFetchIdRef.current = setTimeout(() => setIsFetch(true), noItemsDelay);
         }
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -47,81 +47,76 @@ const SearchActions = () => {
     };
 
     if (text !== '') {
-      timeoutId = setTimeout(fetchData, responseDelay);
+      timeoutIdRef.current = setTimeout(fetchData, responseDelay);
     }
 
     return () => {
-      clearTimeout(timeoutId);
-      clearTimeout(isFetchId);
+      clearTimeout(timeoutIdRef.current);
+      clearTimeout(isFetchIdRef.current);
     };
   }, [text]);
 
   const handleInputChange = event => {
-    clearTimeout(timeoutId);
-    clearTimeout(isFetchId);
+    clearTimeout(timeoutIdRef.current);
+    clearTimeout(isFetchIdRef.current);
     setIsFetch(false);
     setText(event.target.value);
   };
 
   return (
-    <Search>
-      <SearchIconWrapper>
-        <SearchIcon sx={searchIconStyle} />
-      </SearchIconWrapper>
-      <StyledInputBase
-        sx={inputStyles}
-        placeholder=" Пошук . . ."
-        inputProps={{ 'aria-label': 'search' }}
-        value={text}
-        onChange={handleInputChange}
-      />
+      <Search>
+        <SearchIconWrapper>
+          <SearchIcon sx={searchIconStyle} />
+        </SearchIconWrapper>
+        <StyledInputBase
+            sx={inputStyles}
+            placeholder=" Пошук . . ."
+            inputProps={{ 'aria-label': 'search' }}
+            value={text}
+            onChange={handleInputChange}
+        />
 
-      {text !== '' && (
-        <Box
-          sx={searchBlockStyle}
-        >
-          <Stack
-            divider={<Divider orientation="horizontal" sx={{ color: 'black' }} flexItem />}
-            sx={searchBlockInnerStyle}
-          >
-            {(products.length > 0) ? products.map(item => (
-              <NavLink key={item.id} to={`/${item?.categories[0]}/${item?.id}`}>
-                <Stack
-                  key={item.id}
-                  direction="row"
-                  justifyContent="flex-start"
-                  alignItems="center"
-                  spacing={1}
-                >
-                  <ListItemIcon sx={listIconStyle}>
-                    <img
-                      style={productImageStyle}
-                      src={item?.img[0]}
-                      alt="productImage"
-                    />
-                  </ListItemIcon>
-                  <Box sx={boxNameStyle}>{item.name}</Box>
-                </Stack>
-              </NavLink>
-            ))
-              : (isFetch ? (
-                <Box sx={boxResultStyle}>За даним запитом нічого не знайдено. Уточніть свій запит.
-                </Box>
-              ) : (
-                <Box sx={boxResultStyle}>
-                  <CircularProgress color="success" />
-                </Box>
-              ))}
-          </Stack>
-        </Box>
-      )}
-      {text !== '' && (
-      <ClearIcon
-        onClick={() => { setText(''); }}
-        sx={clearIconStyle}
-      />
-      )}
-    </Search>
+        {text !== '' && (
+            <Box sx={searchBlockStyle}>
+              <Stack
+                  divider={<Divider orientation="horizontal" sx={{ color: 'black' }} flexItem />}
+                  sx={searchBlockInnerStyle}
+              >
+                {products.length > 0 ? (
+                    products.map(item => (
+                        <NavLink key={item.id} to={`/${item?.categories[0]}/${item?.id}`}>
+                          <Stack
+                              key={item.id}
+                              direction="row"
+                              justifyContent="flex-start"
+                              alignItems="center"
+                              spacing={1}
+                          >
+                            <ListItemIcon sx={listIconStyle}>
+                              <img
+                                  style={productImageStyle}
+                                  src={item?.img[0]}
+                                  alt="productImage"
+                              />
+                            </ListItemIcon>
+                            <Box sx={boxNameStyle}>{item.name}</Box>
+                          </Stack>
+                        </NavLink>
+                    ))
+                ) : isFetch ? (
+                    <Box sx={boxResultStyle}>За даним запитом нічого не знайдено. Уточніть свій запит.</Box>
+                ) : (
+                    <Box sx={boxResultStyle}>
+                      <CircularProgress color="success" />
+                    </Box>
+                )}
+              </Stack>
+            </Box>
+        )}
+        {text !== '' && (
+            <ClearIcon onClick={() => setText('')} sx={clearIconStyle} />
+        )}
+      </Search>
   );
 };
 
