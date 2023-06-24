676daa7c8fabbd7007f759f1df3d384276b5eab7
IvanBasss <122455382+Basiuk-Ivan@users.noreply.github.com>
2023-06-24 23:28:38 +0300

    connect vite.config

--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -3,4 +3,26 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: './',
+  build: {
+    minify: true,
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'js/[name]-[hash].js',
+        entryFileNames: '[name]-[hash].js',
+
+        assetFileNames: ({ name }) => {
+          if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')) {
+            return 'images/[name]-[hash][extname]';
+          }
+
+          if (/\.css$/.test(name ?? '')) {
+            return '[name]-[hash][extname]';
+          }
+
+          return '[name]-[hash][extname]';
+        }
+      }
+    }
+  }
 });
