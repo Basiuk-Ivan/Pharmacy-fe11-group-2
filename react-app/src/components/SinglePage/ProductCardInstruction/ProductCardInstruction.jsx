9f26b5109a8b7f12b90b7060399e6af60a10cf1d
Artem Shchelinskyi <shchelinskyi@gmail.com>
2023-07-11 08:12:39 +0300

    add tests

--- react-app/src/components/SinglePage/ProductCardInstruction/ProductCardInstruction.jsx
+++ react-app/src/components/SinglePage/ProductCardInstruction/ProductCardInstruction.jsx
@@ -1,90 +1,91 @@
-import { useState, useEffect, useRef } from 'react';
-import { Box, Stack, Typography, Button } from '@mui/material';
-import { scrollClick } from '../../../tools/Scroll/scrollClick';
-import { instructionTextComponentData } from '../../../tools/Instruction/instructionTextComponentData';
-import { instructionButtons } from '../../../tools/Instruction/instructionButtons';
+import {useState, useEffect, useRef} from 'react';
+import {Box, Stack, Typography, Button} from '@mui/material';
+import {scrollClick} from '../../../tools/Scroll/scrollClick';
+import {instructionTextComponentData} from '../../../tools/Instruction/instructionTextComponentData';
+import {instructionButtons} from '../../../tools/Instruction/instructionButtons';
 import TextComponent from './components/TextComponent';
 
-const ProductCardInstruction = ({ productItem }) => {
-  const [activeButton, setActiveButton] = useState(null);
+const ProductCardInstruction = ({productItem}) => {
+    const [activeButton, setActiveButton] = useState(null);
 
-  const compoundRef = useRef(null);
-  const dosageFormRef = useRef(null);
-  const pharmGroupRef = useRef(null);
-  const indicationsRef = useRef(null);
-  const contraindicationsRef = useRef(null);
-  const howToTakeRef = useRef(null);
-  const adverseReactionsRef = useRef(null);
-  const bestBeforeDateRef = useRef(null);
-  const storageConditionsRef = useRef(null);
-  const manufacturerRef = useRef(null);
+    const compoundRef = useRef(null);
+    const dosageFormRef = useRef(null);
+    const pharmGroupRef = useRef(null);
+    const indicationsRef = useRef(null);
+    const contraindicationsRef = useRef(null);
+    const howToTakeRef = useRef(null);
+    const adverseReactionsRef = useRef(null);
+    const bestBeforeDateRef = useRef(null);
+    const storageConditionsRef = useRef(null);
+    const manufacturerRef = useRef(null);
 
-  const arrRef = [
-    compoundRef,
-    dosageFormRef,
-    pharmGroupRef,
-    indicationsRef,
-    contraindicationsRef,
-    howToTakeRef,
-    adverseReactionsRef,
-    bestBeforeDateRef,
-    storageConditionsRef,
-    manufacturerRef
-  ];
+    const arrRef = [
+        compoundRef,
+        dosageFormRef,
+        pharmGroupRef,
+        indicationsRef,
+        contraindicationsRef,
+        howToTakeRef,
+        adverseReactionsRef,
+        bestBeforeDateRef,
+        storageConditionsRef,
+        manufacturerRef
+    ];
 
-  useEffect(() => {
-    window.addEventListener('scroll', () => setActiveButton(false));
-    return () => {
-      window.removeEventListener('scroll', () => setActiveButton(false));
-    };
-  }, [productItem.activeSubstance]);
+    useEffect(() => {
+        window.addEventListener('scroll', () => setActiveButton(false));
+        return () => {
+            window.removeEventListener('scroll', () => setActiveButton(false));
+        };
+    }, [productItem.activeSubstance]);
 
-  return (
-    <Box>
-      <Typography
-        variant="h4"
-        component="h4"
-        gutterBottom
-        sx={{ mt: '68px', mb: '30px', fontSize: '36px', lineHeight: '42px', fontWeight: '700' }}
-      >
-        Інструкція
-      </Typography>
-      <Stack direction="row" spacing={2} flexWrap="wrap" useFlexGap>
-        {instructionButtons.map((button, btnIndex) => (
-          <Button
-            key={button.id}
-            variant="outlined"
-            color={activeButton === button.id ? 'secondary' : 'primary'}
-            onClick={() => scrollClick(button.id)}
-            ref={arrRef[btnIndex]}
-            sx={() => ({
-              color: activeButton === button.id ? '#ffffff' : '#2FD3AE',
-              borderRadius: '50px',
-              border: 'none',
-              backgroundColor: activeButton === button.id ? '#2FD3AE' : '#f7fafb',
-              '&:hover': {
-                backgroundColor: '#2FD3AE',
-                color: '#ffffff',
-                border: 'none'
-              }
-            })}
-          >
-            {button.label}
-          </Button>
-        ))}
-      </Stack>
-      <Box>
-        {instructionTextComponentData(productItem).map(component => (
-          <TextComponent
-            key={component.id}
-            id={component.id}
-            title={component.title}
-            value={component.value}
-          />
-        ))}
-      </Box>
-    </Box>
-  );
+    return (
+        <Box>
+            <Typography
+                variant="h4"
+                component="h4"
+                gutterBottom
+                sx={{mt: '68px', mb: '30px', fontSize: '36px', lineHeight: '42px', fontWeight: '700'}}
+            >
+                Інструкція
+            </Typography>
+            <Stack direction="row" spacing={2} flexWrap="wrap" useFlexGap>
+                {instructionButtons.map((button, btnIndex) => (
+                    <Button
+                        data-testid="btnInstruction"
+                        key={button.id}
+                        variant="outlined"
+                        color={activeButton === button.id ? 'secondary' : 'primary'}
+                        onClick={() => scrollClick(button.id)}
+                        ref={arrRef[btnIndex]}
+                        sx={() => ({
+                            color: activeButton === button.id ? '#ffffff' : '#2FD3AE',
+                            borderRadius: '50px',
+                            border: 'none',
+                            backgroundColor: activeButton === button.id ? '#2FD3AE' : '#f7fafb',
+                            '&:hover': {
+                                backgroundColor: '#2FD3AE',
+                                color: '#ffffff',
+                                border: 'none'
+                            }
+                        })}
+                    >
+                        {button.label}
+                    </Button>
+                ))}
+            </Stack>
+            <Box>
+                {instructionTextComponentData(productItem).map(component => (
+                    <TextComponent
+                        key={component.id}
+                        id={component.id}
+                        title={component.title}
+                        value={component.value}
+                    />
+                ))}
+            </Box>
+        </Box>
+    );
 };
 
 export default ProductCardInstruction;
---
+++ react-app/src/components/SinglePage/ProductCardInstruction/ProductCardInstruction.test.jsx
@@ -0,0 +1,24 @@
+import { render, fireEvent } from '@testing-library/react';
+import ProductCardInstruction from './ProductCardInstruction';
+
+const mockProductItem = {
+  activeSubstance: 'exampleActiveSubstance',
+};
+describe('ProductCardInstruction', () => {
+  test('renders without errors', () => {
+    render(<ProductCardInstruction productItem={mockProductItem} />);
+  });
+
+  test('handles button click correctly', () => {
+    const { getByText } = render(<ProductCardInstruction productItem={mockProductItem} />);
+    const button = getByText('Склад');
+
+    fireEvent.click(button);
+  });
+
+  test('renders text components correctly', () => {
+    const { getByText } = render(<ProductCardInstruction productItem={mockProductItem} />);
+
+    const textComponent = getByText('Інструкція');
+  });
+});
